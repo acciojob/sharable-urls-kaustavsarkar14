65177db00b6c28a7078d4229fa977c6de44ae3e8
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-06-10 22:47:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
  function updateURL() {
       var name = document.getElementById("name").value;
       var year = document.getElementById("year").value;
-      var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
+      var url = "https://localhost:8080/?name=" + name + "&year=" + year;
       document.getElementById("url").textContent = url;
     }
