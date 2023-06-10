df3143a7d0468285494bd7b6c2f2905b0fde7227
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-06-10 22:58:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // your code here
- function updateURL() {
+document.addEventListener("DOMContentLoaded", function() {
+      var form = document.getElementById("myForm");
+      form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        updateURL();
+      });
+    });
+
+    function updateURL() {
       var name = document.getElementById("name").value;
       var year = document.getElementById("year").value;
-      var url = "https://localhost:8080/?name=" + name + "&year=" + year;
+      var url = "https://localhost:8080/?name=" + encodeURIComponent(name) + "&year=" + encodeURIComponent(year);
       document.getElementById("url").textContent = url;
     }
